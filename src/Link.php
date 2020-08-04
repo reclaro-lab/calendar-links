5e314c396afd4dfae00b62400868b7094c0e5a30
Tim Wiffen <tim@reclaro.com>
2020-08-04 13:24:20 +0100

    Revert "First test commit"
    This reverts commit 0a265b0c34fd2ff16642949bf547cb7e2fdad1ab.

--- src/Link.php
+++ src/Link.php
@@ -37,9 +37,6 @@ class Link
     /** @var string */
     protected $address;
 
-    /** @var string */
-    protected $recurPeriod;
-
     public function __construct(string $title, DateTimeInterface $from, DateTimeInterface $to, bool $allDay = false)
     {
         $this->title = $title;
